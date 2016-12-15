f8265e962355d77849b157f59773c8690d74b574
joshkamm <joshkamm@umich.edu>
2016-12-15 12:41:56 -0500

    Learning how to use open babel to create 2D depictions of molecules

--- Python/test_SVD.py
+++ Python/test_SVD.py
@@ -1,11 +1,51 @@
-from subprocess import run
+from subprocess import run, call
 import numpy as np
+import pybel as pb
+from pathlib import Path
+import os
+import glob
 
-run(['obspectrophore','-i','react1.xyz','-i','react2.xyz'])
-print()
+# try calculating a couple spectrophores using the command line utility
+# run(['obspectrophore','-i','react1.xyz','-i','react2.xyz'])
 
-u,s,v = np.linalg.svd([[0.690715, 0.685874],[0.503314,0.792463]])
+# compute a sample spectrophore of a molecule using the pybel and open babel APIs
+testDataPath = Path.home() / 'Molecules' / 'TestMLData'
+os.chdir(str(testDataPath))
+molecule = next(pb.readfile('xyz', 'react3.xyz', None))
+spectrophoreCalculator = pb.ob.OBSpectrophore()
+# print('Spectrophore:', spectrophoreCalculator.GetSpectrophore(molecule.OBMol))
 
-print(u,'\n',s,'\n',v)
+# Try using open babel command line interface to create png's
+print('Path:', os.environ['PATH'])
+call('/Users/joshkamm/miniconda3/bin/obabel react*.xyz -O testMolecules.png -xd -xp 1000', shell=True)
+run(['open', 'testMolecules.png'])
 
-print('DONE!')
+# compute some sample spectrophores and determine the nearest neighbors within them
+molecules = []
+spectrophores = []
+# molecule.write('svg','testMolecules.svg',overwrite=True)
+conv = pb.ob.OBConversion()
+conv.SetInAndOutFormats('xyz','png')
+emptyList = ['']
+# conv.FullConvert(['abc','def'],'testMolecules.png',['react' + str(i) + '.xyz' for i in range(1,7)])
+outPbFile = pb.Outputfile('svg','testMolecules.svg',overwrite=True)
+for i in range(1,7):
+    currentMolecule = next(pb.readfile('xyz', 'react' + str(i) + '.xyz', None))
+    molecules.append(currentMolecule)
+#     outPbFile.write(currentMolecule)
+    spectrophores.append(spectrophoreCalculator.GetSpectrophore(currentMolecule.OBMol))
+outPbFile.close()
+# print(spectrophores)
+
+# trying out stuff with using molecular fingerprints
+# print('Available fingerprints:')
+# print(pybel.fps)
+# print('\nMolecule:')
+# print(molecule.write('smi'))
+# print(molecule.calcfp(fptype='maccs'))
+
+# trying out SVD
+# u,s,v = np.linalg.svd([[0.690715, 0.685874],[0.503314,0.792463]])
+# print(u,'\n',s,'\n',v)
+
+print('DONE WITHOUT ERROR')
